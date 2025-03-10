7bef2b87dae62dd980db689c9b8d9f3cc5333780
Jindra Lacko <jindra@jla-data.net>
2025-03-10 12:21:09 +0100

    let's make future great again!

--- tests/testthat/test-5-vignette.R
+++ tests/testthat/test-5-vignette.R
@@ -6,22 +6,21 @@ test_that("pesky troublemaker", {
                            destination = c("Los Angeles CA", "Austin TX",
                                            "Chicago IL", "Philadelphia PA"),
                            mode = "bicycling",
-                           dep_date = "2025-02-19",
+                           dep_date = as.character(Sys.Date() + 2),
                            dep_time = "22:20:00",
                            key = Sys.getenv("GOOGLE_API_KEY"))
 
   expect_equal(unique(as.vector(results$Status)), "OK")
   expect_equal(any(is.na(as.vector(results$Time))), FALSE)
 
+  karel <- paste(as.character(Sys.Date() + 2), "22:20:00 CET") # date as text
 
   results <- gmapsdistance(origin = c("Washington DC", "New York NY",
                                       "Seattle WA", "Miami FL"),
                            destination = c("Los Angeles CA", "Austin TX",
                                            "Chicago IL", "Philadelphia PA"),
                            mode = "bicycling",
-                           # departure time as seconds from Unix Epoch (1970-01-01)
-                           # check that it happens in future: as.POSIXct(1710000000, origin="1970-01-01")
-                           departure = 1740000000,
+                           departure = as.numeric(as.POSIXct(karel)), # text as UNIX timestamp
                            key = Sys.getenv("GOOGLE_API_KEY"))
 
   expect_equal(unique(as.vector(results$Status)), "OK")
